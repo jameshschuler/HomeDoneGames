c1889ef96a9b1a8776097fa336a81855a2eceb7a
jameshschuler1 <jameshschuler1@gmail.com>
2020-01-06 23:59:42 -0500

    initial commit

---
+++ PoolHouseStudio.HomeDoneGames/Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace PoolHouseStudio.HomeDoneGames.Controllers
+{
+    [ApiController]
+    [Route("api/account")]
+    public class AccountController : ControllerBase
+    {
+        private readonly ILogger<AccountController> _logger;
+
+        public AccountController(ILogger<AccountController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
